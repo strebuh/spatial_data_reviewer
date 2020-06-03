877c6ee6e2637a756bc2315b262f8a1917c7b73f
superHubert <hubert.smieciuch@gmail.com>
2020-06-03 02:05:29 +0200

    get_map simplification and bucketing adjustments done

--- scripts/get_interactive_map.R
+++ scripts/get_interactive_map.R
@@ -4,178 +4,118 @@ library(highcharter)
 library(RColorBrewer)
 library(classInt)
 
+
 get_interactive_map <- function(
-  plot_data,                                   # data frame with data, each row one polygon equivalent
-  map_json,                                   # json file holding a map, to be ploted (.json)
-  # mapline_json_list = NULL,                          # json obszarow zabezpieczenia, jezeli nie podane, nie ryzuje wartosci konturow dodatkowych
-  zmienna_mapowana,                             # index zmiennej do mapowania 
+  plot_data,                                  # frame with data (variables)
+  map_json,                                   # spatial object  - json_list (special for highcharter)
+  mapped_variable,                            # index of variable for mapping (always 4) in this setting
   joining_var = "jpt_kod_je",
-  ilosc_grup = NULL,                                   # na ile pobucketowac
-  # lista_winietek,                                # zmienne na hover klucz lista[nazwa_zmiennej] <- "teskt na hover"
-  tytul = " ",                                  # tytul do wykresu 
-  etykiety_obszarow = FALSE,                    # czy pokazac nazwy obszarow
-  # kolor_granic_map = "",                        # kolor granic obszarow podstawowych (map)
-  # kolor_granic_mapline = "black",               # kolor granic obszarow dodatkowych (mapline)
-  bucketing_seed,   # seed if bclust or kmeans
-  tryb_podzialu = "hclust",                     # hclust, kmeans, sd
+  groups_quantity = NULL,                     # nmber of groups to be created in map
+  title,                                      # map title
+  bucketing_seed,                             # seed if bclust or kmeans
+  bucketing_type = "hclust",                  # bucketing algorithm
   breaks = c(),
-  paleta_kolorow = "BuPu",                        # nazwa palety do mapowania, albo wektor kolorow o ilosci kolorow rownej zmiennej ilosc_group
-  reverse_palette=FALSE,                        # odwroc kolejnosc nasycenia kolorow palety
-  # zmienna_punkty = NULL,                      # zmienna wyznaczajaca punkty
-  nazwa_oddzialow = "Oddziały neurologiczne"    # etykieta
+  colors_palette = "BuPu",                    # coloring palette name
+  reverse_palette=FALSE                       # reverse palette
 ){
   
   # --------------------------------------------------------------------
   
   # informacja o zmiennej ktora jest mapowana
-  message(paste0("Zmienna mapowana to: ", names(plot_data)[zmienna_mapowana]))
+  message(paste0("Mapped varisle is: ", names(plot_data)[mapped_variable]))
   
-  
-  # przygotuj tekst winietek (hover)
-  # if(is.character(lista_winietek)){
-  #   # jezeli teksty sas wjuz w odpowiedzniej formie, zmien tylko nazwe zmiennej mapowanej na value
-  #   text_na_hover <- lista_winietek
-  #   names(plot_data)[zmienna_mapowana] <- "value" 
-  # } else {
-    # # zmien nazwe zmiennej do mapowania na value w winietkach
-    # names(lista_winietek)[names(lista_winietek)==names(plot_data)[zmienna_mapowana]] <- 'value'
-    # # zmien w danych nazwe zmiennej mapowanej
-    org_name <- names(plot_data)[zmienna_mapowana]  
-    names(plot_data)[zmienna_mapowana] <- "value" 
-    # # wybierz zmienne do hovera (pozostale)
-    # zmienne_na_hover <- intersect(names(lista_winietek), names(plot_data))
-    # przygotuj jeden tekst na hover
-    # text_na_hover <- paste0(
-    #   sapply(zmienne_na_hover, function(x){
-    #     paste0(lista_winietek[x],": {point.", x,"}")}),
-    #   collapse = " <br> ")
+  # prepare hover
+    org_name <- names(plot_data)[mapped_variable]  
+    names(plot_data)[mapped_variable] <- "value" 
     text_na_hover <- paste0("{point.Nazwa}<br>", org_name, ": {point.value}")
-    # }
+
   
   # --------------------------------------------------------------------
   
-  # przgotuj zmienna do mapowania
-  zmienna <- plot_data$value
+  # variable
+  variable <- plot_data$value
 
   # --------------------------------------------------------------------
 
   # split on classes 
-  if(is.null(ilosc_grup)){
-    if(tryb_podzialu == "kmeans" | tryb_podzialu == "bclust") set.seed(bucketing_seed)
-    klasy <- classIntervals(zmienna, style=tryb_podzialu)
-    ilosc_grup <- length(klasy$brks) - 1 
-  } else if((tryb_podzialu == "sd" | tryb_podzialu == "pretty") & (length(klasy$brks)!=ilosc_grup)){
+  if(is.null(groups_quantity)){
     
-    # for sd and pretty function may change n, thus update n
-    klasy <- classIntervals(zmienna, ilosc_grup, style=tryb_podzialu)
-    ilosc_grup <- length(klasy$brks) - 1 
-    warning(paste(tryb_podzialu, "has changed number of groups to", ilosc_grup))
+    # in automatic mode you can still give seed for these two modes
+    if(bucketing_type == "kmeans" | bucketing_type == "bclust") set.seed(bucketing_seed)
     
-  } else if(tryb_podzialu == "fixed"){
+    # classes computed withoug given number
+    klasy <- classIntervals(variable, style = bucketing_type)
+    groups_quantity <- length(klasy$brks) - 1
     
-    # custom_breaks <- c(min(zmienna), breaks, max(zmienna))
-    print(custom_breaks)
-    klasy <- classIntervals(zmienna, style=tryb_podzialu, fixedBreaks = breaks)
-    ilosc_grup <- length(breaks) - 1
-    
-  } else if(tryb_podzialu == "kmeans" | tryb_podzialu == "bclust"){
-    # if kmeans or bclust set seed (if not given, then it's null)
-    set.seed(bucketing_seed)
-    klasy <- classIntervals(zmienna, ilosc_grup, style=tryb_podzialu)
-  }
-    
-  # przygotuj kolory dla wartosci zmiennej
-  # jezeli 1 element, to nazwa palety, wybierze z niej ilosc kolorow
-  if(length(paleta_kolorow)==1){
-    tryCatch(kolory <- brewer.pal(ilosc_grup, paleta_kolorow),
-             finally = {
-               kolory <- colorRampPalette(brewer.pal(ilosc_grup, paleta_kolorow))(ilosc_grup) 
-             }
-             )
-    }
+    } else if(bucketing_type == "fixed"){
       
-  # } else if(length(paleta_kolorow)!=1 | length(paleta_kolorow)!=ilosc_grup){
-  #   # jezeli dlugosc wektora to nie 1 i nie ilosc grup, to zle podana paleta
-  #   warning("Podana ilość kolorów nie odpowiada ilości grup, albo paleta nie ma tylu kolorów.")
-  # } # czy odwrocic nasycenie kolorow
-  if(reverse_palette==TRUE){
-    kolory <- rev(kolory)
+      # for fix compute classes using custom breaks
+      klasy <- classIntervals(variable, style=bucketing_type, fixedBreaks = breaks)
+      groups_quantity <- length(breaks) - 1
+    
+    } else {
+      
+      # if kmeans or bclust set seed
+      if(bucketing_type == "kmeans" | bucketing_type == "bclust") set.seed(bucketing_seed)
+      
+      # compute classes
+      klasy <- classIntervals(variable, groups_quantity, style = bucketing_type)
+      
+      # if sd, pretty but algorithm chose another number of classes adjust number of classes
+      if(bucketing_type == "sd" | bucketing_type == "pretty" & length(klasy$brks) != groups_quantity ){
+        
+        groups_quantity <- length(klasy$brks) - 1
+        warning(paste(bucketing_type, "has changed number of groups to", groups_quantity))
+        }
+      }
+    
+
+     # based on palette number pick appropriate number of colors, if necesarry, extend palette
+      tryCatch(colors <- brewer.pal(groups_quantity, colors_palette),
+               finally = {
+                 colors <- colorRampPalette(brewer.pal(groups_quantity, colors_palette))(groups_quantity) 
+               }
+               )
+      
+      
+    # if colors palette to be reverted
+    if(reverse_palette == TRUE){
+    colors <- rev(colors)
   }
     
-  # tabela z przedzialami i kolorami
-  tabela.kolorow <- findColours(klasy, kolory)
+  # table of colors
+  colors.table <- findColours(klasy, colors)
   
   # --------------------------------------------------------------------
   
-  # zasiegi przedzialow wyznaczone przez findColours
-  zakresy <- names(attr(tabela.kolorow, "table"))
+  # ranges of colors from colors.table
+  ranges <- names(attr(colors.table, "table"))
   
-  # wytnij dolne i gorne granice przedzialow z zasiegow przedzialow
-  od <- str_replace_all(zakresy, "\\[|,(-)?\\d+(\\.\\d+)?(\\)|\\])", "")
-  do <- str_replace_all(zakresy, "\\[|(-)?\\d+(\\.\\d+)?,|(\\)|\\])", "")
+  # extract number form ranges strings
+  from <- str_replace_all(ranges, "\\[|\\(|,(-)?\\d+(\\.\\d+)?(\\)|\\])", "")
+  to <- str_replace_all(ranges, "\\[|\\(|(-)?\\d+(\\.\\d+)?,|(\\)|\\])", "")
   
-  # tabela od,do,kolor
-  tabela.kolorow <- data.frame(from = as.numeric(od),
-                               to = as.numeric(do),
-                               color = kolory)
-  tabela.kolorow_lista <- list_parse(tabela.kolorow)
-  
-  # # --------------------------------------------------------------------
-  # # punkty szpitali
-  # if(!is.null(zmienna_punkty)){
-  #   baza <- plot_data[zmienna_punkty != 0,]
-  #   orig_coords <- data.frame(lat=as.double(baza$latitude), lon=as.double(baza$longitude))
-  #   
-  #   coordinates(orig_coords) <- c('lon', 'lat')
-  #   proj4string(orig_coords) <- CRS("+init=epsg:4326")
-  #   
-  #   # Zamiana lon/lat na EPSG:3857 - taka sama, jak projekcja w mapce
-  #   Metric_coords <- spTransform(orig_coords, CRS("+init=epsg:3857"))
-  #   
-  #   # Stworzenie nowej bazy ze współrzędnymi i nazwą szpitala
-  #   geo_oddz <- data.frame(lat=Metric_coords$lat, lon=Metric_coords$lon, name = baza$name, oddzial = baza$n_odzialow)
-  #   
-  #   # Stworzenie geojsona na podstawie nowej bazy
-  #   map_geojson <-  geojsonio::geojson_json(geo_oddz, lat = "lat", lon = "lon")
-  # }
+  # new table of colors
+  colors.table <- data.frame(from = as.numeric(from),
+                               to = as.numeric(to),
+                               color = colors)
+  colors.table_list <- list_parse(colors.table)
   
   # --------------------------------------------------------------------
-  mapka <- highchart(type= "map") %>%
+  map <- highchart(type= "map") %>%
     hc_add_series(mapData = map_json,
                   type = "map",
                   name = "",
                   data = plot_data,
                   joinBy = joining_var,
                   value="value",
-                  # geojson = TRUE
                   showInLegend = TRUE,
-                  # color = kolor_granic_map, # nie dziala tutaj, nie znalazlem jak wstawic
                   tooltip = list(pointFormat = text_na_hover)) %>%
-    hc_colorAxis(dataClasses = tabela.kolorow_lista) %>%
-    hc_title(text=tytul,
+    hc_colorAxis(dataClasses = colors.table_list) %>%
+    hc_title(text=title,
              fontWeight = "bold") %>%
     hc_mapNavigation(enabled = TRUE) 
-    
-  #   if(!is.null(zmienna_punkty)){
-  #     mapka <-  hc_add_series(mapka, mapData = map_json,
-  #                   type='mappoint',
-  #                   dataLabels = list(enabled = etykiety_obszarow), # bez podpisow
-  #                   showInLegend = FALSE,
-  #                   data = map_geojson,
-  #                   name = nazwa_oddzialow,
-  #                   tooltip = list(pointFormat = "{point.name}"))
-  #     
-  #   }
-  # 
-  # if(!is.null(mapline_json_list)){
-  #   mapka <-  hc_add_series(mapka, data = mapline_json_list, 
-  #                           type = "mapline", 
-  #                           color = kolor_granic_mapline,
-  #                           name = "", 
-  #                           geojson = TRUE)
-  # }
-
   
-  return(mapka)
+  return(map)
 }
 
